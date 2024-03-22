7d372696feaa3dccc610f195091c423c935580dd
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2024-03-22 15:56:41 +0100

    style : Added Footer Style

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import styles from './footer.module.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>Agency</div>
+      <div className={styles.text}>
+       © Copyright 2024 Agency One.All Rights Reserved.
+      </div>
+    </div>
   )
 }
 
