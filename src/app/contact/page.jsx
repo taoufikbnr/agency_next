dee4c4b27c78dabc636d52b162e7ec2c998e320f
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2024-03-22 20:10:25 +0100

    style: Added Contact page style

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import Image from "next/image";
+import styles from "./contact.module.css";
 
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src={"/about_bg.webp"} alt='about' fill className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="fullname"/>
+          <input type="text" placeholder="email"/>
+          <input type="text" placeholder="phone number"/>
+          <textarea name="" id="" cols="30" rows="10"placeholder="message"></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/blog-card/blogCard.jsx
@@ -0,0 +1,9 @@
+import styles from './blog.module.css'
+
+const blogCard = () => {
+  return (
+    <div>blogCard</div>
+  )
+}
+
+export default blogCard
