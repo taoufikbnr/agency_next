079caf7103d939215b175281f69adeda7230e324
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2024-03-22 19:53:14 +0100

    style: Added about page style

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,12 +3,29 @@ import styles from './about.module.css'
 
 const AboutPage = () => {
   return (
-    <div>
-      <h1>about</h1>
-      <div className={styles.imgContainer}>
-        <Image src={"https://images.pexels.com/photos/3082341/pexels-photo-3082341.jpeg"} alt='about' fill />
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h3 className={styles.subtitle}>About Agency</h3>
+        <h1 className={styles.title}>text</h1>
+        <p className={styles.desc} >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi porro cupiditate sed tenetur impedit, nemo vel iusto autem. Asperiores nulla animi sed velit. Cupiditate, dolorem modi. Quae tenetur veritatis id.</p>
+        <div className={styles.stats}>
+          <div className={styles.stat}>
+            <h1>+10</h1>
+            <p>Years of experience</p>
+          </div>
+          <div className={styles.stat}>
+            <h1>+10</h1>
+            <p>Years of experience</p>
+          </div>
+          <div className={styles.stat}>
+            <h1>+10</h1>
+            <p>Years of experience</p>
+          </div>
+        </div>
       </div>
-      <blockquote>hello</blockquote>
+      <div className={styles.imgContainer}>
+        <Image src={"/about_bg.webp"} alt='about' fill className={styles.img} />
+      </div>
     </div>
   )
 }
